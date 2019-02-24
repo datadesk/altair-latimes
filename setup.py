b99baea0cf0b930bd9d9d1318abd5ffa584f49eb
Ben Welsh <ben.welsh@gmail.com>
2019-02-24 07:22:24 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='altair-latimes',
+    version='0.0.1',
+    description="A Los Angeles Times theme for Python's Altair statistical visualization library",
+    author='The Los Angeles Times Data Desk',
+    author_email='datadesk@latimes.com',
+    url='http://www.github.com/datadesk/altair-latimes/',
+    license="MIT",
+    packages=('altair_latimes',),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
